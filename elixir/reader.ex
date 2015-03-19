5341c993cbaf800c6bc736cdb5655df3eb92b0e2
Akira Hayakawa <ruby.wktk@gmail.com>
2015-03-19 11:01:59 +0900

    update
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- elixir/reader.ex
+++ elixir/reader.ex
@@ -17,27 +17,51 @@ defmodule MAL do
 
   # toks :: list of string
   def read_form(toks) do
+    elem(parse_form(toks), 0)
+  end
+
+  def parse_form(toks) do
     case toks do
-      ["(", _] -> read_list(toks)
-      [tok, _] -> read_atom(tok)
+      ["(" | _] -> parse_list(toks)
+      [tok | _] -> parse_atom(toks)
     end
   end
 
-  # ((1),2) -> {:list, [{:list, [{:int, 1}]}, {:int, 2}]}
-  def read_list(toks) do
+  # ((1) 2) -> {{:list, [{:list, [{:int, 1}]}, {:int, 2}]}, []]}
+  def parse_list(toks) do
+    [_ | tl] = toks
+    {:mal_list, elem(do_parse_list([], tl), 0)}
   end
 
-  # tok :: string
-  def read_atom(tok) do
-    cond do 
+  # :: {[ast], toks}
+  def do_parse_list(acc, toks) do
+    case toks do
+      [")" | rest] -> 
+        {acc, rest}
+      true ->
+        {ast, rest} = parse_form(toks)
+        do_parse_list([ast | acc], rest)
+    end
+  end
+
+  # 1 2) -> {{:int, 1}, ["2", ")"]}
+  def parse_atom(toks) do
+    [hd | rest] = toks
+    tok = hd # FIXME trim
+    ast = cond do 
       Integer.parse(tok) != :error ->
         {:mal_number, elem(Integer.parse(tok), 0)}
-      true -> tok
+      true ->
+        {:mal_symbol, tok}
     end
+    {ast, rest}
   end
 end
 
-MAL.tokenizer("(+ 12 3)") |> IO.inspect
+MAL.tokenizer("(+ 123 456)") |> IO.inspect
 MAL.tokenizer("(+ 2 (* 3 4))") |> IO.inspect
 
-MAL.read_atom("1") |> IO.inspect
+MAL.parse_form(["(", ")"]) |> IO.inspect
+MAL.parse_form(["(", "1", ")"]) |> IO.inspect
+MAL.parse_form(["1", ")"]) |> IO.inspect
+MAL.parse_form(["*", "1", "2", ")"]) |> IO.inspect
