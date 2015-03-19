1aad96328b3c140da4be1bf2c9b98ea339b7cb00
Akira Hayakawa <ruby.wktk@gmail.com>
2015-03-20 08:28:12 +0900

    step2: basic work
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- elixir/step2_eval.ex
+++ elixir/step2_eval.ex
@@ -2,30 +2,46 @@ import MAL.Reader
 import MAL.Printer
 
 defmodule MAL.Main do
-  def repl_env, do: {
-    '+' => fn a, b -> a + b,
-  }
+  def repl_env do
+    %{
+      "+" => fn args ->
+        [x|[y|_]] = args
+        {:mal_number, elem(x, 1) + elem(y, 1)}
+      end
+    }
+  end
 
   def read(str), do: MAL.Reader.read_str(str)
 
-  # :: ast
   def eval_ast(ast, env) do
     case ast do
       {:mal_symbol, name} ->
-        # lookup
+        case env[name] do
+          nil -> raise ArgumentError, message: "symbol not found"
+          x -> x
+        end
       {:mal_list, xs} ->
-        {:mal_list, Enum.map(xs, fn x -> eval(x, env))}
+        {:mal_list, Enum.map(xs, fn x -> eval(x, env) end)}
       _ -> ast
     end
   end
 
   def eval(ast, env) do
+    IO.inspect ast
+    case ast do
+      {:mal_list, xs} ->
+        l = eval_ast(ast, env)
+        {:mal_list, [f | args]} = l
+        f.(args)
+      _ -> eval_ast(ast, env)
+    end
   end
 
   def print(exp), do: MAL.Printer.pr_str(exp)
   def rep(line) do
     a = read line
-    b = eval(a, "")
+    b = eval(a, repl_env)
+    IO.inspect b
     c = print b
     c
   end
