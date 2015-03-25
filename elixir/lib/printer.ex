0cf448642c1a9e4c5de27cbac1969e68048964c8
Akira Hayakawa <ruby.wktk@gmail.com>
2015-03-25 18:42:16 +0900

    add pr param to pr_str
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- elixir/lib/printer.ex
+++ elixir/lib/printer.ex
@@ -1,13 +1,17 @@
 defmodule MAL.Printer do
+
+  def unescape(chr) do
+  end
+
   @spec pr_str(MAL.Types.t) :: String.t
-  def pr_str(ast) do
+  def pr_str(ast, pr \\ false) do
     case ast do
       {:mal_func, _} -> "#<function>"
       {:mal_list, val} ->
-        l = val |> Enum.map(&(pr_str(&1))) |> Enum.join(" ")
+        l = val |> Enum.map(&(pr_str(&1, pr))) |> Enum.join(" ")
         "(" <> l <> ")"
       {:mal_vector, val} ->
-        l = val |> Enum.map(&(pr_str(&1))) |> Enum.join(" ")
+        l = val |> Enum.map(&(pr_str(&1, pr))) |> Enum.join(" ")
         "[" <> l <> "]"
       {:mal_int, val} -> to_string(val)
       {:mal_symbol, val} -> val
@@ -15,7 +19,23 @@ defmodule MAL.Printer do
       {:mal_bool, true} -> "true"
       {:mal_bool, false} -> "false"
       {:mal_nil} -> "nil"
-      {:mal_string, val} -> "\"#{val}\""
+      {:mal_string, val} ->
+        case pr do
+          true -> "\"#{
+            val |>
+            to_char_list |>
+            Enum.map(fn chr ->
+                  case chr do
+                    '\n' -> "\\n"
+                    '\\' -> "\\\\"
+                    '"' -> "\\\""
+                    c -> to_string([c])
+                  end
+                end)
+            |> to_string
+          }\""
+          false -> val
+        end
     end
   end
 end
@@ -24,3 +44,4 @@ IO.puts MAL.Printer.pr_str({:mal_func, 1})
 IO.puts MAL.Printer.pr_str({:mal_int, 1})
 IO.puts MAL.Printer.pr_str({:mal_list, [{:mal_int, 1},{:mal_int, 2}]})
 IO.puts MAL.Printer.pr_str({:mal_vector, [{:mal_int, 1},{:mal_int, 2}]})
+IO.puts MAL.Printer.pr_str({:mal_string, "akiradeveloper"})
