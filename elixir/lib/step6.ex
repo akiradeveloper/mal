c3a9bf6a1a182b2f9d2fd8a5971c7fc1682b1fc4
Akira Hayakawa <ruby.wktk@gmail.com>
2015-03-26 19:25:12 +0900

    fix: missing wrap_func
    Signed-off-by: Akira Hayakawa <ruby.wktk@gmail.com>

--- elixir/lib/step6.ex
+++ elixir/lib/step6.ex
@@ -90,13 +90,12 @@ defmodule MAL.Step6 do
 
   def main do
     env = MAL.Core.init_env
-    MAL.Env.set(env, "eval", fn [ast] -> eval(ast, env) end)
+    MAL.Env.set(env, "eval", fn [ast] -> eval(ast, env) end |> wrap_func)
     (read "(def! not (fn* (a) (if a false true)))") |> eval(env)
     (read "(def! load-file (fn* (f) (eval (read-string (str \"(do \"(slurp f) \")\")))))") |> eval(env)
     Stream.repeatedly(fn ->
       line = IO.gets "user> "
       (read line) |> eval(env) |> print |> IO.puts
     end) |> Stream.run
-    # loop(env)
   end
 end
